853033728ce9abe58338a916cf52105a12e6d80a
lee DJ <sky32133@naver.com>
2022-05-11 13:40:10 +0900

    연습

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace Cs_3
         class Dog  // 1번 클래스 사용 
         {
             protected int eyes, nose, mouse, ears;             //클래스에서 사용할 변수 선언
-            public void bark()                                  //1. 클래스 매개변수 호출해보기
+            virtual public void bark()                                  //1. 클래스 매개변수 호출해보기
             {
                 Console.WriteLine("멍멍");
             }
@@ -25,6 +25,18 @@ namespace Cs_3
                 this.mouse = mouse;
                 this.ears = ears;
             }
+
+            //오버 로딩 동일 이름을 갖고 있을경우, 매개변수에 따라서 함수 출력이 달라짐 
+            public void doginpo(string name){
+                Console.WriteLine("name = {0}", name);
+            }
+
+            public void doginpo(string name, int sex){
+                Console.WriteLine("name = {0} \nsex = {1}", name,sex);
+            }
+
+
+
             public void show()
             {
                 Console.WriteLine("{0}, {1}, {2}, {3}", eyes, nose, mouse, ears);  //str이 아니라 변수의 경우 각각 자리를 포매팅 해줘야함.
@@ -64,28 +76,34 @@ namespace Cs_3
             }
 
         }
-        class buldok : Dog{  //2번 상속
-            private string kind;        
-            public buldok(){            // dog() private -> public or protected / priavet 상태에서는 상속이 불가 
-                                        // public = 어디서든 접근이 가능.
-                                        // protected = 상속관계일 때 접근이 가능
-                                        // private = 해당 클래스에서만 접근이 가능
+        class buldok : Dog
+        {  //2번 상속
+            private string kind;
+            public buldok()
+            {            // dog() private -> public or protected / priavet 상태에서는 상속이 불가 
+                         // public = 어디서든 접근이 가능.
+                         // protected = 상속관계일 때 접근이 가능
+                         // private = 해당 클래스에서만 접근이 가능
                 base.eyes = 0;
                 base.nose = 0;
                 base.mouse = 0;
                 base.ears = 0;
                 this.kind = "불독";
             }
-            public void buldogshow(){
-                Console.WriteLine("{0}, {1}, {2}, {3} {4}", eyes, nose, mouse, ears,kind);  //str이 아니라 변수의 경우 각각 자리를 포매팅 해줘야함.
+            public void buldogshow()
+            {
+                Console.WriteLine("{0}, {1}, {2}, {3} {4}", eyes, nose, mouse, ears, kind);  //str이 아니라 변수의 경우 각각 자리를 포매팅 해줘야함.
             }
-            public void buldogbark(){
+            public override void bark()
+            {
                 Console.WriteLine("으르롱");
             }
-            public string getkind(){
+            public string getkind()
+            {
                 return this.kind;
             }
-            public void setkind(string kind){
+            public void setkind(string kind)
+            {
                 this.kind = kind;
             }
 
@@ -109,9 +127,14 @@ namespace Cs_3
             buldok bul = new buldok();
             bul.buldogshow();
             bul.bark();
-            bul.buldogbark();
+            bul.bark();
             bul.setkind("주민우");
             bul.buldogshow();
+
+            //3번 오버라이딩, 오버로딩
+            Dog dog3 = new Dog();
+            dog3.doginpo("박준우");
+            dog3.doginpo("권헌진",1);
 
         }
     }
